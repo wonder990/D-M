02c3a78bcd14a940d6116ceba980146c7d04446e
wonder990 <gustavofernandezcalizaya@gmail.com>
2022-09-17 19:17:24 -0400

    button burguer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,24 @@ import '../header/header.css'
 
 const Header = () => {
   const [linkActive, setlinkActive] = useState()
+  const [buttonActive, setbuttonActive] = useState(false)
+  console.log(buttonActive)
   return (
-    <header className="header-inicio">
+    <header className="header-inicio ">
       <h2 className="logo">D&N</h2>
-      <ul>
+      <ul
+        className={
+          buttonActive
+            ? 'pr-0 duration-300 bg-[#FAFAFA] lg:bg-inherit w-full flex justify-center items-center flex-col absolute top-[80px] lg:relative lg:flex lg:justify-center lg:items-center lg:top-0 lg:flex-row lg:pr-[150px] lg:w-auto'
+            : 'pr-0 duration-300 -top-[500px] lg:bg-inherit absolute w-full flex justify-center items-center flex-col lg:relative lg:flex lg:justify-center lg:items-center lg:top-0 lg:flex-row lg:pr-[150px] lg:w-auto'
+        }
+      >
         <li>
           <a
-            onClick={() => setlinkActive('Inicio')}
+            onClick={() => {
+              setbuttonActive(false)
+              setlinkActive('Inicio')
+            }}
             className={linkActive == 'Inicio' ? 'active-Link' : 'inactive-Link'}
             href="#Inicio"
           >
@@ -18,7 +29,10 @@ const Header = () => {
         </li>
         <li className="link-l">
           <a
-            onClick={() => setlinkActive('Soluciones')}
+            onClick={() => {
+              setbuttonActive(false)
+              setlinkActive('Soluciones')
+            }}
             className={
               linkActive == 'Soluciones' ? 'active-Link' : 'inactive-Link'
             }
@@ -29,7 +43,10 @@ const Header = () => {
         </li>
         <li className="link-r">
           <a
-            onClick={() => setlinkActive('Nosotros')}
+            onClick={() => {
+              setbuttonActive(false)
+              setlinkActive('Nosotros')
+            }}
             className={
               linkActive == 'Nosotros' ? 'active-Link' : 'inactive-Link'
             }
@@ -40,7 +57,10 @@ const Header = () => {
         </li>
         <li>
           <a
-            onClick={() => setlinkActive('Contacto')}
+            onClick={() => {
+              setbuttonActive(false)
+              setlinkActive('Contacto')
+            }}
             className={
               linkActive == 'Contacto' ? 'active-Link' : 'inactive-Link'
             }
@@ -50,6 +70,18 @@ const Header = () => {
           </a>
         </li>
       </ul>
+      <button
+        className="z-30 lg:hidden"
+        onClick={() => {
+          setbuttonActive(!buttonActive)
+        }}
+      >
+        {buttonActive ? (
+          <i className="text-[30px] fa-solid fa-xmark"></i>
+        ) : (
+          <i className="text-[30px] fa-solid fa-bars"></i>
+        )}
+      </button>
     </header>
   )
 }
