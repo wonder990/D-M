7b3d3c51d160456ff2bfb24ef0f0b2c719b367ea
wonder990 <gustavofernandezcalizaya@gmail.com>
2022-09-17 19:28:28 -0400

    links de redireccion

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -9,7 +9,7 @@ const Inicio = () => {
     <main id="Inicio" className="presentation">
       <h2>Transformá la forma en la que tu equipo se relaciona</h2>
       <p>Trabajemos en el cambio que estás buscando.</p>
-      <a href="#solutions">Elevá tus resultados</a>
+      <a href="#nosotros">Contacto</a>
     </main>
   )
 }
